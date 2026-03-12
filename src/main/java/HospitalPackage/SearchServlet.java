e66daffb5c4a14114a0cda13a8a75a85294133c3
VishwaNadeen <vishwanadeen1795@gmail.com>
2026-03-13 03:44:15 +0530

    new updated

--- src/main/java/HospitalPackage/SearchServlet.java
+++ src/main/java/HospitalPackage/SearchServlet.java
@@ -3,21 +3,6 @@
 import java.io.IOException;
 import java.util.List;
 
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-@WebServlet("/SearchServlet")
-public class SearchServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-
-	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String id=request.getParameter("id");
-		
 		
 /*	try {
 		List<PatientModel> Details = PatientCtrl.validate(id);
